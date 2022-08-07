98a1b3019c2c35f50b4f74f1004effb5132a5f9b
SamuelDAlencar <sarl.2001@hotmail.com>
2022-08-07 16:47:51 -0300

    style: estilizacao geral dos repositorios
    adiciona topicos, simbolo de public, coloracao geral, divisao por bordas, header, linguagem

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -1,12 +1,30 @@
-import PropTypes from "prop-types"
-import React, { useContext } from 'react';
-import UserContext from '../contexts/UserContext';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 export default function Repository({ repository }) {
   console.log(repository);
 
   return (
-    <h1>{ repository.name }</h1>
+    <section className='user_rep_section'>
+      <section className='user_repTitle_section'>
+        <a
+          href={repository.html_url}
+          className='user_repTitle_a'
+        >
+          {repository.name}
+        </a>
+        <span>Public</span>
+      </section>
+      <p className='user_repDescription_p'>{repository.description}</p>
+      <section className='user_repTopics_section'>
+        {repository.topics && (
+          repository.topics.map((topic, i) => {
+            return <p key={i} className='user_repTopic_p'>{topic}</p>;
+          })
+        )}
+      </section>
+      <p className='user_repLanguage_p'>{repository.language}</p>
+    </section>
   );
 }
 Repository.propTypes = {
